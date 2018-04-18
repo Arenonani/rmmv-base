4df4a46927f1035d2387877ea100fb412968f2ba
Are <arenonani@arenonani.com>
2018-04-18 15:25:32 +0900

    Improve touch UI

--- js/rpg_core.js
+++ js/rpg_core.js
@@ -3045,6 +3045,10 @@ TouchInput.isPressed = function() {
     return this._mousePressed || this._screenPressed;
 };
 
+TouchInput.isMousePressed = function() {
+    return this._mousePressed;
+};
+
 /**
  * Checks whether the left mouse button or touchscreen is just pressed.
  *
